f6b779c6f541a8c2f3ec37e81e929fac3d2cd4c8
PakThet <pakthetsr@gmail.com>
2025-11-17 20:34:33 +0700

    update code

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,17 +6,32 @@
         </div>
     @endif 
     <a href="{{ route('products.create') }}" class="btn btn-primary">Create Product</a>
-    <p>================================</p>
-    @foreach ($products as $product)
-    <h2><a href="{{ route('products.show', $product->id) }}" class="btn btn-outline-primary">View</a></h2>
-        <h2>{{ $product->name }}</h2>
-        <p>{{ $product->description }}</p>
-        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-outline-success">Edit</a>
-        <form action="{{ route('products.destroy', $product) }}" method="post" onsubmit="return confirm('Are you sure you want to delete this product?');">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-outline-danger">Delete</button>
-        </form>
-        <p>================================</p>
-    @endforeach
+    <table class="table table-striped">
+        <tr>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Size</td>
+            <td>Action</td>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->description }}</td>
+            <td>{{ $product->size }}</td>
+            <td>
+                <div class="d-flex gap-2">
+                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-outline-primary">View</a>
+                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-outline-success">Edit</a>
+                    <form action="{{ route('products.destroy', $product) }}" method="post" onsubmit="return confirm('Are you sure you want to delete this product?');">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-outline-danger">Delete</button>
+                    </form>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {{ $products->links('pagination::bootstrap-5') }}
 </x-layout>
